5c3780f2ba755f1fff0295c0031a84009518351e
nathanrobinson <nathan@Nathans-MacBook-Pro.local>
2016-03-27 19:46:36 -0500

    Initial commit
     Changes to be committed:
    	new file:   app/app.component.ts
    	new file:   app/home.component.ts
    	new file:   app/main.ts
    	new file:   app/people/people.component.ts
    	new file:   app/people/person.component.ts
    	new file:   app/people/person.service.ts
    	new file:   index.html
    	new file:   package.json
    	new file:   styles.css
    	new file:   tsconfig.json
    	new file:   typings.json
    	new file:   views/app.html
    	new file:   views/home.html
    	new file:   views/people.html
    	new file:   views/person.html

---
+++ app/home.component.ts
@@ -0,0 +1,6 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  templateUrl: './views/home.html'
+})
+export class HomeComponent { }
---
+++ app/people/person.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from 'angular2/core';
+export class Person {
+  constructor(public id: number, public name: string) { }
+}
+@Injectable()
+export class PersonService {
+  getPeople() { return peoplePromise; }
+  getPerson(id: number | string) {
+    return peoplePromise
+      .then(people => people.filter(h => h.id === +id)[0]);
+  }
+  updatePerson(person: Person){}
+  addPerson(person: Person){
+      let newId = Math.max.apply(null, PEOPLE.map(person => person.id)) + 1;
+      person.id = newId;
+      peoplePromise.then(people => people.push(person));
+      return person;
+  }
+}
+var PEOPLE = [
+    new Person(11, 'Mr. Nice'),
+    new Person(12, 'Narco'),
+    new Person(13, 'Bombasto'),
+    new Person(14, 'Celeritas'),
+    new Person(15, 'Magneta'),
+    new Person(16, 'RubberMan')
+];
+var peoplePromise = Promise.resolve(PEOPLE);
